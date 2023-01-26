bc6db4c11e6875423ef780055d9b6b7c80696c87
DAM2Alu1 <210162@iessierradeguara.com>
2023-01-26 12:32:07 +0100

    Proyecto mongo nuevo

--- Mongo_Java/src/_04a_RetrievingAllDocuments.java
+++ Mongo_Java/src/_04a_RetrievingAllDocuments.java
@@ -30,12 +30,12 @@ public class _04a_RetrievingAllDocuments {
 		
 		// DOCUMENTO 1
 		// El documento 1 se ha creado en la clase anterior
-		/* Document document1 = new Document("title", "MongoDB")
-		.append("description", "database")
-		.append("likes", 100)
-		.append("url", "http://www.tutorialspoint.com/mongodb/")
-		.append("by", "tutorials point");
-		*/
+			/* Document document1 = new Document("title", "MongoDB")
+			.append("description", "database")
+			.append("likes", 100)
+			.append("url", "http://www.tutorialspoint.com/mongodb/")
+			.append("by", "tutorials point");
+			*/
 		//DOCUMENTO 2
 		Document document2 = new Document("title", "RethinkDB")
 		.append("description", "database")
@@ -43,16 +43,17 @@ public class _04a_RetrievingAllDocuments {
 		.append("url", "http://www.tutorialspoint.com/rethinkdb/")
 		.append("by", "tutorials point");
 		
+		
 		List<Document> list = new ArrayList<Document>();
 		//list.add(document1);
 		list.add(document2);
 		collection.insertMany(list);
 		
-		// Getting the iterable object
+		// Obtener el objeto iterable
 		FindIterable<Document> iterDoc = collection.find();
 		int i = 1;
 		
-		// Getting the iterator
+		// Obtener el iterador
 		Iterator it = iterDoc.iterator();
 		while (it.hasNext()) {
 			System.out.println(it.next());
---
+++ Proyecto_Mongodb/src/Ejercicios/Ciudad.java
@@ -0,0 +1,65 @@
+package Ejercicios;
+
+public class Ciudad {
+  private String name;
+  private String country;
+  private String timezone;
+  private long population;
+  private float longitude;
+  private float latitude;
+  
+  
+
+  public Ciudad(String name, String country, String timezone, long population, float longitude, float latitude) {
+	super();
+	this.name = name;
+	this.country = country;
+	this.timezone = timezone;
+	this.population = population;
+	this.longitude = longitude;
+	this.latitude = latitude;
+}
+public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public String getTimezone() {
+    return timezone;
+  }
+  public void setTimezone(String timezone) {
+    this.timezone = timezone;
+  }
+
+  public long getPopulation() {
+    return population;
+  }
+
+  public void setPopulation(long population) {
+    this.population = population;
+  }
+
+  public float getLongitude() {
+    return longitude;
+  }
+  public void setLongitude(float longitude) {
+    this.longitude = longitude;
+  }
+
+  public float getLatitude() {
+    return latitude;
+  }
+  public void setLatitude(float latitude) {
+    this.latitude = latitude;
+  }
+}
+
